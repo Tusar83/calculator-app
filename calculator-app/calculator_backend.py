162316b77fc3b81123c33b1d1436b0be62e0cf7f
Tusar <tusarkantameher83@gmail.com>
2026-08-11 23:14:38 +0400

    Update calculator logic1

--- calculator-app/calculator_backend.py
+++ calculator-app/calculator_backend.py
@@ -60,5 +60,8 @@ def read_root():
     return {"message": "Calculator API is running! Use POST /calculate to calculate"}
 
 if __name__ == "__main__":
+    import os
+
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
